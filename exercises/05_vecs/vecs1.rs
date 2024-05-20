dd14d8a3f08f21a08e35be2a8b39c5995d598e04
Faith Kimani <wanjikukimani24@gmail.com>
2024-05-20 18:13:43 +0000

    Complete vecs1.rs

--- exercises/05_vecs/vecs1.rs
+++ exercises/05_vecs/vecs1.rs
@@ -11,8 +11,9 @@
 
 fn array_and_vec() -> ([i32; 4], Vec<i32>) {
     let a = [10, 20, 30, 40]; // a plain array
-    let v = // TODO: declare your vector here with the macro for vectors
+    let mut v = vec![10,20,30,40];// TODO: declare your vector here with the macro for vectors
 
+    println!("{:?}", a);
     (a, v)
 }
 
