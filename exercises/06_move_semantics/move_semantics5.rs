da87d95aa23ac6ad8a4f874e9b7216d8fcc3d358
Faith Kimani <wanjikukimani24@gmail.com>
2024-05-28 18:12:04 +0000

    use mutable reference and dereferencing to edit the original (x) value

--- exercises/06_move_semantics/move_semantics5.rs
+++ exercises/06_move_semantics/move_semantics5.rs
@@ -6,14 +6,13 @@
 // Execute `rustlings hint move_semantics5` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
 
 #[test]
 fn main() {
     let mut x = 100;
-    let y = &mut x;
-    *y += 100;
-    let z = &mut x;
+    let y = &mut x;// this is pointing to the memory location of x. (y) itself doesn't have the value but the mem location
+    *y += 100;//this accesses the actual value pointed by y
+    let z = &mut x;// this refers to the updated value of x after y's operation
     *z += 1000;
     assert_eq!(x, 1200);
 }
